efd73e9cb094654e16a5cd4a8b48bbe8fcc7b5dc
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-06-18 14:38:40 +0900

    Initial Commit

---
+++ chapter30.7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chapter30.7
+//
+//  Created by HEE TAE YANG on 2020/06/18.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
